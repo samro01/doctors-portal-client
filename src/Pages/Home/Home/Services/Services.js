516a47e0eedcf55b6b95cfa854d277417f1af4ea
samro01 <srozario22@gmail.com>
2022-11-12 15:40:14 +0100

    first commit

---
+++ src/Pages/Home/Home/Services/Services.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import fluoride from '../../../../assets/images/fluoride.png';
+import cavity from '../../../../assets/images/cavity.png';
+import whitening from '../../../../assets/images/whitening.png';
+import Service from './Service';
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            id: 1,
+            name: 'Flouride Treatement',
+            description: 'lorem ipsum',
+            img: fluoride
+        },
+        {
+            id: 2,
+            name: 'Cavity Treatement',
+            description: 'lorem ipsum',
+            img: cavity
+        },
+        {
+            id: 3,
+            name: 'Whiteningent',
+            description: 'lorem ipsum',
+            img: whitening
+        },
+
+    ]
+
+    return (
+        <div className='mt-16'>
+            <div className='text-center'>
+                <h3 className='text-xl font-bold text-primary uppercase'>Our Services</h3>
+                <h2 className='text-3xl'>Services We Provide</h2>
+            </div>
+            <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    servicesData.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
